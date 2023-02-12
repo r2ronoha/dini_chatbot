2ef513bef94df9877ef8f643c4847ab193abef7f
Arturo Noha <arturo.noha@wandera.com>
2023-02-12 15:12:04 +0000

    print

--- joke_chatbot.py
+++ joke_chatbot.py
@@ -6,14 +6,14 @@ age = int(age)
 if 1 <= age <= 3:
     print("What stays in the corner but can travel the world?")
     for i in range(10):
-        print(".")
+        print('.', end='', flush=True)
         sleep(1)
     print("A stamp")
 
 elif 4 <= age <= 6:
     print("What begins with T has Tea in the middle and ends with T?")
     for i in range(10):
-        print(".")
+        print('.', end='', flush=True)
         sleep(1)
     print("Teapot")
 
@@ -24,6 +24,6 @@ elif 7 <= age <= 9:
         \nfourth crocodiles in the ocean \
         \nwhich one is the safest? ")
     for i in range(10):
-        print(".")
+        print('.', end='', flush=True)
         sleep(1)
     print("Crocodiles don't live in the ocean.")
